408524942098129111f48de61922e8557629da8c
reubenbauer <bauer.reuben@gmail.com>
2017-04-20 19:14:10 -0500

    add FOIAed contact card level data

--- load.R
+++ load.R
@@ -19,9 +19,13 @@
 #---------------------------- Section 1: Preliminaries -------------------------
 
 #-------- set repository ----
-# data_repository <- "C:/Users/reubenbauer/Box Sync/cpd_stops/data/"
+data_repository <- "C:/Users/reubenbauer/Dropbox (Team Steve)/"
 # data_repository <- "C:/Users/reuben_bauer/Box Sync/Box Sync/cpd_stops/data/"
 
 #---------------------------- Section 2: Load Data -----------------------------
-crime <- fread("C:/Users/reubenbauer/Box Sync/cpd_stops/data/Crimes_-_2001_to_present.csv", header = TRUE, stringsAsFactors = FALSE)
-stops <- read.csv("FOIA PO55346 Response.csv", header = TRUE, stringsAsFactors = FALSE)
+crime <- fread(paste0(data_repository,"crimes_2001_present/Crimes_-_2001_to_present.csv"), header = TRUE, stringsAsFactors = FALSE)
+stops <- read.csv(paste0(data_repository, "FOIA PO55346/FOIA PO55346 Response.csv"), header = TRUE, stringsAsFactors = FALSE)
+cc12 <- read.csv(paste0(data_repository, "FOIA PO59333/2012CONTACTCARDS.csv"), header = TRUE, stringsAsFactors = FALSE)
+cc13 <- read.csv(paste0(data_repository, "FOIA PO59333/2013CONTACTCARDS.csv"), header = TRUE, stringsAsFactors = FALSE)
+cc14 <- read.csv(paste0(data_repository, "FOIA PO58150/ContactCardData2014.csv"), header = TRUE, stringsAsFactors = FALSE)
+cc15 <- read.csv(paste0(data_repository, "FOIA PO58150/ContactCardData2015.csv"), header = TRUE, stringsAsFactors = FALSE)
